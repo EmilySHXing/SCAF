58447049c9d4b7f0edea4548ec42bf1d116cce5c
Shuhao Xing <xing35@purdue.edu>
2020-09-17 19:40:21 +0900

    home page format

--- app/src/main/java/com/example/scaf/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/scaf/ui/home/HomeFragment.java
@@ -1,6 +1,12 @@
 package com.example.scaf.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,7 +20,11 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.scaf.R;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
+
+import static android.graphics.Typeface.BOLD;
 
 public class HomeFragment extends Fragment {
 
@@ -25,9 +35,36 @@ public class HomeFragment extends Fragment {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.date);
-        String currentDate = java.text.DateFormat.getDateTimeInstance().format(new Date());
-        textView.setText(currentDate);
+        final TextView dateTextView = root.findViewById(R.id.date);
+        final TextView monthTextView = root.findViewById(R.id.month);
+        Date date = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd\nE\nyyyy", Locale.US);
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MMM", Locale.US);
+        String currentDate = dateFormat.format(date);
+        String currentMonth = monthFormat.format(date);
+        Spannable dateSpan = new SpannableString(currentDate);
+        dateSpan.setSpan(
+                new ForegroundColorSpan(Color.RED),
+                3,6,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        dateSpan.setSpan(
+                new RelativeSizeSpan(0.7f),
+                3,6,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        dateSpan.setSpan(
+                new StyleSpan(BOLD),
+                0,2,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        dateSpan.setSpan(
+                new RelativeSizeSpan(2.5f),
+                0,2,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        dateSpan.setSpan(
+                new RelativeSizeSpan(0.8f),
+                7,11,
+                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        dateTextView.setText(dateSpan);
+        monthTextView.setText(currentMonth);
         /*
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
