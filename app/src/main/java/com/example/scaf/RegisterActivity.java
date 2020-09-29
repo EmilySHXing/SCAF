1305bfc023f5de306befd91827c9920778978d27
Kaiwen <kwyyyu@gmail.com>
2020-09-29 02:53:26 -0400

    finish login and signup

--- app/src/main/java/com/example/scaf/RegisterActivity.java
+++ app/src/main/java/com/example/scaf/RegisterActivity.java
@@ -1,23 +1,90 @@
 package com.example.scaf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.scaf.ui.login.LoginActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    FirebaseAuth mFirebaseAuth;
+    EditText usernameEditText;
+    EditText nameEditText;
+    EditText petnameEditText;
+    EditText passwordEditText;
+    Button signupButton;
+    TextView loginTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText nameEditText = findViewById(R.id.name);
-        final EditText petnameEditText = findViewById(R.id.petname);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button signupButton = findViewById(R.id.signup);
+        usernameEditText = findViewById(R.id.username);
+        nameEditText = findViewById(R.id.name);
+        petnameEditText = findViewById(R.id.petname);
+        passwordEditText = findViewById(R.id.password);
+        signupButton = findViewById(R.id.signup);
+        loginTextView = findViewById(R.id.loginlink);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText().toString();
+                String name = nameEditText.getText().toString();
+                String petname = petnameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.isEmpty()) {
+                    usernameEditText.setError("Please enter an email address!");
+                    usernameEditText.requestFocus();
+                }
+                else if (password.isEmpty()) {
+                    passwordEditText.setError("Please enter a password!");
+                    passwordEditText.requestFocus();
+                }
+                else if (password.length()<8) {
+                    passwordEditText.setError("Password needs to at least 8 characters!");
+                    passwordEditText.requestFocus();
+                }
+                else {
+                    mFirebaseAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(RegisterActivity.this, "Sign up unsuccessful, please try again!", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(RegisterActivity.this, MainActivity.class));
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+        loginTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/scaf/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/scaf/ui/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.scaf.ui.login;
 
 import android.app.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -14,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
@@ -28,23 +30,84 @@ import com.example.scaf.R;
 import com.example.scaf.RegisterActivity;
 import com.example.scaf.ui.login.LoginViewModel;
 import com.example.scaf.ui.login.LoginViewModelFactory;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginViewModel loginViewModel;
+    FirebaseAuth mFirebaseAuth;
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button loginButton;
+    TextView registerText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
-                .get(LoginViewModel.class);
 
-        final EditText usernameEditText = findViewById(R.id.username);
-        final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
-        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
-        final TextView registerText = findViewById(R.id.signuplink);
+        usernameEditText = findViewById(R.id.username);
+        passwordEditText = findViewById(R.id.password);
+        loginButton = findViewById(R.id.login);
+        registerText = findViewById(R.id.signuplink);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        FirebaseAuth.AuthStateListener mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
+                if (mFirebaseUser != null) {
+                    Toast.makeText(LoginActivity.this, "You are logged in!", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please log in.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (username.isEmpty()) {
+                    usernameEditText.setError("Please enter an email address!");
+                    usernameEditText.requestFocus();
+                }
+                else if (password.isEmpty()) {
+                    passwordEditText.setError("Please enter a password!");
+                    passwordEditText.requestFocus();
+                }
+                else if (password.length()<8) {
+                    passwordEditText.setError("Password is at least 8 characters!");
+                    passwordEditText.requestFocus();
+                }
+                else {
+                    Log.d("email", username);
+                    Log.d("password", password);
+                    mFirebaseAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "Sign In unsuccessful, please try again!", Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            }
+                        }
+                    });
+
+                }
+            }
+        });
 
         registerText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,96 +118,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
-            @Override
-            public void onChanged(@Nullable LoginFormState loginFormState) {
-                if (loginFormState == null) {
-                    return;
-                }
-                loginButton.setEnabled(loginFormState.isDataValid());
-                if (loginFormState.getUsernameError() != null) {
-                    usernameEditText.setError(getString(loginFormState.getUsernameError()));
-                }
-                if (loginFormState.getPasswordError() != null) {
-                    passwordEditText.setError(getString(loginFormState.getPasswordError()));
-                }
-            }
-        });
-
-        loginViewModel.getLoginResult().observe(this, new Observer<LoginResult>() {
-            @Override
-            public void onChanged(@Nullable LoginResult loginResult) {
-                if (loginResult == null) {
-                    return;
-                }
-                loadingProgressBar.setVisibility(View.GONE);
-                if (loginResult.getError() != null) {
-                    showLoginFailed(loginResult.getError());
-                }
-                if (loginResult.getSuccess() != null) {
-                    updateUiWithUser(loginResult.getSuccess());
-                }
-                setResult(Activity.RESULT_OK);
-
-                //Complete and destroy login activity once successful
-                finish();
-            }
-        });
-
-        TextWatcher afterTextChangedListener = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                // ignore
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // ignore
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                loginViewModel.loginDataChanged(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        };
-        usernameEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.addTextChangedListener(afterTextChangedListener);
-        passwordEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    loginViewModel.login(usernameEditText.getText().toString(),
-                            passwordEditText.getText().toString());
-                }
-                return false;
-            }
-        });
-
-
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 
-    private void updateUiWithUser(LoggedInUserView model) {
-        String welcome = getString(R.string.welcome) + model.getDisplayName();
-        // TODO : initiate successful logged in experience
-        Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
-    }
-
-    private void showLoginFailed(@StringRes Integer errorString) {
-        Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
-    }
 
 }
