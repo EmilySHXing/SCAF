2857583f059cf89be60452a6b6b6b744813a92a6
Shuhao Xing <xing35@purdue.edu>
2020-10-09 12:08:45 +0900

    implement functions

--- app/src/main/java/com/example/scaf/Functions.java
+++ app/src/main/java/com/example/scaf/Functions.java
@@ -1,10 +1,16 @@
 package com.example.scaf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.scaf.ui.functions.FunctionsFragment;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Functions extends AppCompatActivity {
 
